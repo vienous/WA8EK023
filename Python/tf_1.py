006f3112b9dd21a8987420b5acbe63967b36133d
vienous <kafusky@hotmail.com>
2017-11-15 14:35:00 +0800

    add some java

--- Python/tf_1.py
+++ Python/tf_1.py
@@ -1,6 +1,13 @@
 import tensorflow as tf;
-hello = tf.constant('hello,TensorFlow')
+hello = tf.constant('a')
 
 sess = tf.Session();
 
-sess.run(hello);
+print("##############")
+
+print(sess.run(hello));
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a+b))
+
